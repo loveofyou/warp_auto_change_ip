bfe9099ac8670853be1d6f54441673db4ee5796d
loveofyou <loveofyous@gmail.com>
2022-12-23 17:24:24 +0800

    Update warp_change_ip.sh

--- warp_change_ip.sh
+++ warp_change_ip.sh
@@ -6,13 +6,13 @@
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
 read -r -p "Is warp installed? [y/n] " input
 if [[ "$input" == "n" ]];then
-    bash <(curl -fsSL https://github.com/luoxue-bot/warp.sh/raw/main/warp.sh) 4
+    bash <(curl -6 -fsSL https://github.com/luoxue-bot/warp.sh/raw/main/warp.sh) 4
 elif [[ "$input" == "y" ]];then
     read -r -p "Input the region you want(e.g. HK,SG):" area
 fi
 while [[ "$input" == "y" ]]
 do
-    result=$(curl --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" 2>&1)
+    result=$(curl -6 --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" 2>&1)
     if [[ "$result" == "404" ]];then
         echo -e "Originals Only, Changing IP..."
         systemctl restart wg-quick@wgcf
@@ -24,7 +24,7 @@ do
         sleep 3
 	
     elif  [[ "$result" == "200" ]];then
-		region=`tr [:lower:] [:upper:] <<< $(curl --user-agent "${UA_Browser}" -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | cut -d '/' -f4 | cut -d '-' -f1)` ;
+		region=`tr [:lower:] [:upper:] <<< $(curl -6 --user-agent "${UA_Browser}" -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | cut -d '/' -f4 | cut -d '-' -f1)` ;
 		if [[ ! "$region" ]];then
 			region="US";
 		fi
